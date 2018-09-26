91955c24890526ba03e42a45411c4ce589483c48
Scott Semyan <scottse@microsoft.com>
2018-09-26 18:26:56 +0200

    Initial checkin.

---
+++ SimpleFrontEnd/js/mainscript.js
@@ -0,0 +1,84 @@
+﻿AZ.Documents = (function (ko) {
+	"use strict";
+
+	var docViewModel = {
+		documents: ko.observableArray(),
+		currentUser: ko.observable()
+	};
+
+	var apiUrl = "";
+
+	function getApiData() {
+
+		// Get list of Documents for the current user (as determined by the API)
+		AZ.Ajax.MakeAjaxCall("GET",
+			apiUrl,
+			null,
+			function (data) {
+				initList(data.Items, data.UserName);
+			});
+	}
+
+	function initList(items, username) {
+		docViewModel.documents = ko.observableArray(items);
+		docViewModel.currentUser(username);
+		ko.applyBindings(docViewModel);
+	}
+
+	// Do this on start
+	$(document).ready(function () {
+
+		// Set the URL based in whether we are running locally or not
+		// The URL locations are set in vars.js
+		// This is a hack and is better solved via a build process
+		if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+			apiUrl = localUrl;
+		} else {
+			apiUrl = remoteUrl;
+		}
+
+		// Now get the data
+		if (noApi) {
+			initList(null, 'Local User');
+		} else {
+			getApiData();
+		}
+	});
+
+	return {
+		model: docViewModel,
+
+		AddEdit: function() {
+			var newTodo = prompt("Enter new todo item:");
+			if (newTodo) {
+				var newTodoItem = { ItemName: newTodo }
+				if (noApi) {
+					newTodoItem.ItemCreateDate = new Date();
+					docViewModel.documents.push(newTodoItem);
+				} else {
+					AZ.Ajax.MakeAjaxCall("POST",
+						apiUrl,
+						JSON.stringify(newTodoItem),
+						function (data) {
+							docViewModel.documents.push(data);
+						});
+				}
+			}
+		},
+
+		Remove: function(item) {
+			if (confirm("Mark item '" + item.ItemName + "' as completed?")) {
+				if (noApi) {
+					docViewModel.documents.remove(item);
+				} else {
+					AZ.Ajax.MakeAjaxCall("DELETE",
+						apiUrl + "/" + item.id,
+						null,
+						function (data) {
+							docViewModel.documents.remove(item);
+						});
+				}
+			}
+		}
+	}
+}(ko));
---
+++ SimpleFrontEnd/js/vars.js
@@ -0,0 +1,6 @@
+﻿// If noApi is set to true, the API will not be called
+var noApi = true;
+
+// Put the urls to your local api and your remote API here
+var localUrl = "http://localhost:7071/api/todoitem";
+var remoteUrl = "https://<YOUR_FUNCTIONS_URL>/api/todoitem";
